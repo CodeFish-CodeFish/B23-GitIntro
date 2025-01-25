bcfa0407471bafa39c006a4d82a42abeed766596
zhamil <chynarayuss@gmail.com>
2025-01-25 12:45:54 -0600

    Jamil

--- src/test/java/com/gitPractice/TestClass.java
+++ src/test/java/com/gitPractice/TestClass.java
@@ -4,9 +4,13 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("some code goes here");
+            int n=10; int a = 1; int b = 2;
+        for (int i = 0; i < n; i++) {
+           int sum = a+b;
+            b = a; b= sum;
+            System.out.println(sum);
 
-        for (int i = 0; i < 5; i++) {
-
+            System.out.println("Jamil's code");
         }
     }
 }
