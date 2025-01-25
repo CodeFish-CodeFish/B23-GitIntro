8d9c51b3fc745349edeac59b65eb028cf4b339d7
codefish <contact@codefish.io>
2025-01-25 12:44:54 -0600

    resolved conflicts

--- src/test/java/com/gitPractice/TestClass.java
+++ src/test/java/com/gitPractice/TestClass.java
@@ -4,11 +4,11 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("some code goes here");
-
-        for (int i = 0; i < 5; i++) {
-
-
-            System.out.println("change made by Vivian");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                System.out.println("change made by Vivian");
+            }
         }
     }
 }
